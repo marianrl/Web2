b5f25d906693f1d3e401d743d65a5e2863bac4b8
DESKTOP-ND87ERP\Mariano <marianoh.rodriguez@davinci.edu.ar>
2021-04-18 17:18:36 -0300

    Commit de prueba

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hola</title>
+</head>
+<body>
+<p>Hola Mundo</p>
+<?php
+    echo date('Y-m-d');
+    $a = 10;
+    echo $a;
+ ?>
+
+    
+</body>
+</html>
